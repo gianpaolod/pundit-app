f921bc83e8f288327a668311b76b3c5bf881a801
Gianpaolo D'Amato <damatohg@gmail.com>
2017-07-07 13:53:34 +0000

    Added prodiles controller, new view, and link on navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,20 @@
-15.times do |n|
-  email = "example-#{n+1}@example.org"
+User.create!(email: 'admin@admin.com', password: 'password', password_confirmation: 'password', role: :admin)
+
+10.times do |n|
+  email = "example-user#{n+1}@example.org"
   password = "password"
   User.create!(
                email:                 email,
                password:              password,
                password_confirmation: password)
+end
+
+5.times do |n|
+  email = "example-editor#{n+1}@example.org"
+  password = "password"
+  User.create!(
+               email:                 email,
+               password:              password,
+               password_confirmation: password,
+               role: :editor)
 end
